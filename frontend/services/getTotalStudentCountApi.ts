b6aa570efeeb9cf6c5acb1e9f1322ddd1bc68f17
rnasante <raynice54@gmail.com>
2025-04-02 21:07:29 +0000

    consumed the correct api to get total students count

--- frontend/services/getTotalStudentCountApi.ts
+++ frontend/services/getTotalStudentCountApi.ts
@@ -1,5 +1,5 @@
 export async function getTotalStudents(): Promise<number> {
-    const response = await fetch("http://localhost:5000/api/students");
+    const response = await fetch("http://localhost:5000/api/students/total");
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
